fd771b9704aac232ab3455bbbad749187b3cb7b6
Yevgeni Boukingolts <Yevgeni.b93@gmail.com>
2025-01-12 15:13:05 +0200

    revert 2

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,9 +5,6 @@ const router = express.Router();
 const authController = require('../controllers/auth.controller');
 const { verifyToken, verifyAdmin } = require('../middleware/auth.middleware');
 
-const passport = require('passport'); // google strategy
-const jwt = require('jsonwebtoken'); // google strategy
-
 // const User = require('../models/user.model');
 
 router.post('/signup', authController.signup);
@@ -21,7 +18,5 @@ router.get('/profile', verifyToken, authController.settings);
 router.post('/refresh', authController.refreshToken);
 
 // router.get('/is-admin', authController.isAdmin)
-
-// router.post('/google-login', authController.googleLogin);
 
 module.exports = router;
