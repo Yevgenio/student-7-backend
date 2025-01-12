ce7629279374679d035c21ba467f1cdb8bbd2272
Yevgeni Boukingolts <Yevgeni.b93@gmail.com>
2025-01-12 17:31:37 +0200

    password not required

--- models/user.model.js
+++ models/user.model.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt');
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String },
   avatar: {type: String, default: "default"},
   role: { type: String, enum: ['user', 'admin'], default: 'user' }
 });
