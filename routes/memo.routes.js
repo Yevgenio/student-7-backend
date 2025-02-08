82743b41985862d817033d232cbf1aae45d79f4f
Yevgeni Boukingolts <Yevgeni.b93@gmail.com>
2025-02-08 02:07:07 +0200

    MEMO UPDATE AND DELETE

--- routes/memo.routes.js
+++ routes/memo.routes.js
@@ -16,16 +16,32 @@ router.get('/id/:id', memoController.getMemoById);
 // GET memos by query
 router.get('/search', memoController.searchMemos);
 
-// // GET distinct categories
-// router.get('/categories', memoController.getDistinctCategories);
-
 // POST new memo
 router.post(
     '/', 
-    // verifyToken, 
-    // verifyAdmin, 
+    verifyToken, 
+    verifyAdmin, 
     upload.fields([{ name: 'imagePath', maxCount: 1 }]), 
     memoController.addNewMemo
 );
+
+// PUT update memo by ID
+router.put(
+    '/id/:id',
+    verifyToken,
+    verifyAdmin,
+    upload.fields([
+      { name: 'imagePath', maxCount: 1 },
+    ]),
+    memoController.updateMemoById
+  );
+
+// DELETE memo by ID
+router.delete(
+    '/id/:id',
+    verifyToken,
+    verifyAdmin,
+    memoController.deleteMemoById
+  );
 
 module.exports = router;
